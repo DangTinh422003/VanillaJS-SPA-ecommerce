baf3d1cf1047cf51ba214549210f3927784e9324
caodangtinh <caodangtinh04022003@gmail.com>
2024-05-05 22:43:36 +0700

    update: cygress spec file

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
 describe("My First Test", () => {
-  it("Does not do much!", () => {
-    expect(true).to.equal(false);
+  it('finds the content "type"', () => {
+    cy.visit("https://example.cypress.io");
   });
 });
